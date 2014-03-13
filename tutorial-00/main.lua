9e5b7a53450d8f7984fc7069a28057f2a29be9ad
Anthony Moralez <amoralez@esri.com>
2014-03-13 12:47:33 -0700

    Created initial tutorial-00

---
+++ tutorial-00/main.lua
@@ -0,0 +1,4 @@
+print("contentWidth: "..display.contentWidth.." contentHeight: "..display.contentHeight)
+lime = require("lime.lime")
+local map = lime.loadMap("tutorial-00.tmx")
+local visual = lime.createVisual(map)
